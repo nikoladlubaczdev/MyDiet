001f6ede954de6ec1bf327019b4aa470adf85ee3
Nikola Dłubacz <nikola.dlubacz@capgemini.com>
2026-05-13 11:12:29 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import React from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-lg font-bold text-gray-800">
+        🎉 NativeWind + Tailwind uszkonfigurowane!
+      </Text>
+      <Text className="text-sm text-gray-500 mt-2">
+        Edytuj App.tsx i zacznij tworzyć
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
